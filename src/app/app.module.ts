5aa9fbae6b55b6969fe83b8f3f9e5f17f0b562c4
Carolina <aenthcr@gmail.com>
2023-09-16 16:29:27 -0600

    Héroes de DBZ Sección 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './Counter/counter.module';
 import { HeroesModule } from './Heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
+
+
 // import { HeroComponent } from './Heroes/hero/hero.component';
 // import { ListComponent } from './Heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
     ],
   imports: [
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
